216bfb06d887bf771759675d7b3cc4c32ebdfb63
Isaiah Iruoha <Siruoha@gmail.com>
2023-03-25 14:26:38 -0400

    Added New Vibility Functionality to All Pages

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ window.onload = function() {
     dashboard.addEventListener('click', function() {
       document.getElementById("menuSidebar").hidden = true
       document.getElementById("menuMiddle").hidden = true
+      document.getElementById("dashboardMiddle").style.display = "flex"
+      document.getElementById("dashboardSidebar").style.display = "flex"
     });
 
-    const alerts = document.getElementById("alerts");
+    const alerts = document.getElementById("podSystem");
     alerts.addEventListener('click', function() {
       document.getElementById("menuSidebar").hidden = true
       document.getElementById("menuMiddle").hidden = true
+      document.getElementById("podsystemMiddle").style.display = "flex"
+      document.getElementById("podsystemSidebar").style.display = "flex"
     });
 
-    const inputs = document.getElementById("inputs");
+    const inputs = document.getElementById("alerts");
     inputs.addEventListener('click', function() {
       document.getElementById("menuSidebar").hidden = true
       document.getElementById("menuMiddle").hidden = true
+      document.getElementById("alertsMiddle").style.display = "flex"
+      document.getElementById("alertsSidebar").style.display = "flex"
     });
 
-    const podSystem = document.getElementById("podSystem");
+    const podSystem = document.getElementById("inputs");
     podSystem.addEventListener('click', function() {
       document.getElementById("menuSidebar").hidden = true
       document.getElementById("menuMiddle").hidden = true
+      document.getElementById("inputsMiddle").style.display = "flex"
+      document.getElementById("inputsSidebar").style.display = "flex"
     });
   };
