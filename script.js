d646458e9997a1bb3ebe6bdf2f3b43bd336ac446
Isaacttchung <isaacttchung@gmail.com>
2023-03-25 17:28:16 -0400

    Added Speedometer to Dashboard

--- script.js
+++ script.js
@@ -32,3 +32,21 @@ window.onload = function() {
     });
   };
   
+  var speedometer = document.getElementById("speedometer");
+var speedometerValue = document.getElementById("speedometer-value");
+var speed = 0;
+var maxSpeed = 180;
+
+function updateSpeedometer() {
+	var angle = speed / maxSpeed * 180;
+	document.querySelector(".speedometer-needle").style.transform = "translateX(-50%) rotate(" + angle + "deg)";
+	speedometerValue.innerHTML = speed + " km/h";
+}
+
+setInterval(function() {
+	speed += Math.floor(Math.random() * 10) + 1;
+	if (speed > maxSpeed) {
+		speed = 0;
+	}
+	updateSpeedometer();
+}, 1000);
