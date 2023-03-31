d61d3a24e9f3a25a1bdb64b52b19c003d664e517
Isaiah Iruoha <Siruoha@gmail.com>
2023-03-31 18:08:25 -0400

    Prepared Dashboard For Work

--- script.js
+++ script.js
@@ -36,24 +36,24 @@ window.onload = function() {
     //end of page visibility
 
     //start of speedometer
-    var speedometer = document.getElementById("speedometer");
-    var speedometerValue = document.getElementById("speedometer-value");
-    var speed = 0;
-    var maxSpeed = 180;
+//     var speedometer = document.getElementById("speedometer");
+//     var speedometerValue = document.getElementById("speedometer-value");
+//     var speed = 0;
+//     var maxSpeed = 180;
 
-function updateSpeedometer() {
-	var angle = speed / maxSpeed * 180;
-	document.querySelector(".speedometer-needle").style.transform = "translateX(-50%) rotate(" + angle + "deg)";
-	speedometerValue.innerHTML = speed + " km/h";
-}
+// function updateSpeedometer() {
+// 	var angle = speed / maxSpeed * 180;
+// 	document.querySelector(".speedometer-needle").style.transform = "translateX(-50%) rotate(" + angle + "deg)";
+// 	speedometerValue.innerHTML = speed + " km/h";
+// }
 
-setInterval(function() {
-	speed += Math.floor(Math.random() * 10) + 1;
-	if (speed > maxSpeed) {
-		speed = 0;
-	}
-	updateSpeedometer();
-}, 1000);
+// setInterval(function() {
+// 	speed += Math.floor(Math.random() * 10) + 1;
+// 	if (speed > maxSpeed) {
+// 		speed = 0;
+// 	}
+// 	updateSpeedometer();
+// }, 1000);
 //end of speedometer 
 
 //Start at local storage conditionals
